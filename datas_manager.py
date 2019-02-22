47958079f2bb13bf2e18d419539447dd761a8dc6
pabissonnier <pabissonnier@gmail.com>
2019-02-22 09:43:22 +0100

    Function that extracts the products is working

--- datas_manager.py
+++ datas_manager.py
@@ -33,22 +33,31 @@ def category_to_url(category_name):
 
 def products_extract(category_name_url):
     """ Extracting JSON products values and converting into list """
-    category_url = "https://fr.openfoodfacts.org/categorie/{0}.json".format(category_name_url)
-    products_json = urllib.request.urlopen(category_url)
-    products_read = products_json.read()
-    products_data = json.loads(products_read.decode("utf-8"))
-
-    key_list = ["product_name", "ingredients_text", "url", "categories" \
-                , "stores_tags" , "nutrition_grades"]
+    i = 1
     products_list = []
-    for content in products_data["products"]:
-        products_list_2 = []
-        for key, value in content.items():
-            if key in key_list:
-                products_list_2.append(value)
-                products_list.append(products_list_2)
-            elif key not in key_list:
-                pass
+    while i <= 5:
+        category_url = "https://fr.openfoodfacts.org/categorie/{0}/{1}.json".format(category_name_url, i)
+        products_json = urllib.request.urlopen(category_url)
+        products_read = products_json.read()
+        products_data = json.loads(products_read.decode("utf-8"))
+
+        for content in products_data["products"]:
+            products_dict = {}
+            for key, value in content.items():
+                if key == "product_name":
+                    products_dict["name"] = value
+                elif key == "ingedients_text":
+                    products_dict["ingredients"] = value
+                elif key == "url":
+                    products_dict["link"] = value
+                elif key == "categories":
+                    products_dict["categories"] = value
+                elif key == "store_tags":
+                    products_dict["stores"] = value
+                elif key == "nutrition_grades":
+                    products_dict["nutriscore"] = value
+            products_list.append(products_dict)
+        i += 1
     print(products_list)
 
 
@@ -61,5 +70,4 @@ def convert_list_tuples(list_name):
 
 
 test = category_to_url("Aliments et boissons à base de végétaux")
-print(test)
 products_extract(test)
